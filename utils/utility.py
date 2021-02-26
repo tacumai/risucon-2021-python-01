d9350f44217aa1fa0e4ec2046bf7c43d6e32c6a7
isucon player <isucon@ip-10-0-0-37.ap-northeast-1.compute.internal>
2021-02-26 05:38:12 +0000

    fix getpassword func

--- utils/utility.py
+++ utils/utility.py
@@ -1,19 +1,25 @@
 from datetime import datetime as dt
-import subprocess
+# import subprocess
 import secrets
-
+import hashlib
 
 def get_salt():
     return secrets.token_hex(64)
 
 
 def get_passwordhash(salt, password):
-    string = password + salt
-    cmd = 'echo -n ' + string + ' | openssl sha256'
-    return  (subprocess.check_output(cmd, shell=True)
-                        .decode('utf-8')
-                        .partition(' ')[2]
-                        .rstrip())
+    #string = password + salt
+    #cmd = 'echo -n ' + string + ' | openssl sha256'
+    #return  (subprocess.check_output(cmd, shell=True)
+    #                    .decode('utf-8')
+    #                    .partition(' ')[2]
+    #                    .rstrip())
+
+    return hashlib.sha256(password+salt).hexdigest()
+            .decode('utf-8')
+            .partition(' ')[2]
+            .rstrip())
+
 
 def get_today():
     now = dt.today()
