22ec2742fa550e5cb296bed5ec027dc585bbb939
isucon player <isucon@ip-10-0-0-37.ap-northeast-1.compute.internal>
2021-02-26 05:43:46 +0000

    fix

--- utils/utility.py
+++ utils/utility.py
@@ -8,14 +8,12 @@ def get_salt():
 
 
 def get_passwordhash(salt, password):
-    #string = password + salt
-    #cmd = 'echo -n ' + string + ' | openssl sha256'
-    #return  (subprocess.check_output(cmd, shell=True)
-    #                    .decode('utf-8')
-    #                    .partition(' ')[2]
-    #                    .rstrip())
+    cmd = 'echo -n ' + password + salt + ' | openssl sha256'
+    return  (subprocess.check_output(cmd, shell=True)
+                        .decode('utf-8')
+                        .partition(' ')[2]
+                        .rstrip())
 
-    return hashlib.sha256(password+salt).hexdigest().decode('utf-8').partition(' ')[2].rstrip()
 
 
 def get_today():
