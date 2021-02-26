e58adb93909374d220cd643244d01694606fee6a
isucon player <isucon@ip-10-0-0-37.ap-northeast-1.compute.internal>
2021-02-26 06:03:51 +0000

    fix bug

--- utils/utility.py
+++ utils/utility.py
@@ -1,20 +1,18 @@
 from datetime import datetime as dt
-# import subprocess
+import subprocess
 import secrets
-import hashlib
 
 def get_salt():
     return secrets.token_hex(64)
 
 
 def get_passwordhash(salt, password):
-    cmd = 'echo -n ' + password + salt + ' | openssl sha256'
+    string = password + salt
+    cmd = 'echo -n ' + string + ' | openssl sha256'
     return  (subprocess.check_output(cmd, shell=True)
                         .decode('utf-8')
                         .partition(' ')[2]
                         .rstrip())
-
-
 
 def get_today():
     now = dt.today()
